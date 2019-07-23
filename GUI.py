3dd4a1f7841ffbe2d5d972b4e8ed5b86b2c1e3d1
daman1209arora <45387992+daman1209arora@users.noreply.github.com>
2019-07-24 01:49:26 +0530

    Finished with connecting load and save buttons

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
 import sys 
-import matplotlib.pyplot as plt
 import pygame
 import math 
 import wave,struct
@@ -9,43 +8,88 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon,QPixmap
 from PyQt5.QtWidgets import QFileDialog,QPushButton,QLabel,QApplication,QSlider,QWidget,QVBoxLayout,QScrollArea,QGroupBox,QHBoxLayout
 
+class SoundPlayer:
+    def __init__(self):
+        pygame.mixer.init()
+        self.isPlaying=False
+        self.player=pygame.mixer
 
-sampleRate=44100
+    def play(self,buffer):
+        if(self.isPlaying==True):
+            self.player.stop() 
+       #else: 
+        file = wave.open('temp.wav','w')
+        file.setnchannels(2)
+        file.setsampwidth(2)
+        file.setframerate(samplingRate)
+        for v in buffer:
+            file.writeframesraw(struct.pack('<h',v ))
+        file.close()
+        self.isPlaying=True
+        self.player.Sound('temp.wav').play(-1)
+
+    def stop(self):
+        if(self.isPlaying==True):
+            self.player.stop()
+            self.isPlaying=False
+
+
+class FileHandler():
+    def writeInFile(self,filename,sliders):
+        file=open(filename,'w')
+        base_freq_txt=str(base_freq)+' '
+        file.write(base_freq_txt)
+        for slider in sliders:
+            txt=str(slider.value())+' '
+            file.write(txt)
+        file.close()
+
+    def readFile(self,filename):
+        file=open(filename,'r')
+        s=file.readlines()[0].split(' ')
+        return (int(s[0]),list(map(int,s[1:len(s)-1])))
+
 
 
 harmonic_slider_width=1000
 #try to set slider to fill entire space available to it if possible
-
-
+app=QApplication(sys.argv)
+ 
 tick_interval=1
-interval=(0,100)
+interval=(0,1000)
 
-soundPlayer=None
-app=QApplication(sys.argv) 
+samplingRate=44100
+base_freq=100
+
+fl=FileHandler()
+soundPlayer=SoundPlayer()
 mainWindow=QWidget()
 playerWindow=QWidget(mainWindow)
 fileWindow=QWidget(mainWindow)
 harmonics=QScrollArea()
 harmonicSliders=[]
+
 #playerWindow,harmonics and fileWindow go on top of the mainWindow. Make better GUI arrangements in terms of layout
+def stop():
+    soundPlayer.stop()
 
-isPlaying=False
+def play():
+    b=createSoundBuffer(base_freq)
+    soundPlayer.play(b)
 
-def stop(q):
-    global soundPlayer,isPlaying
-    if(isPlaying==True):
-        isPlaying=False
-        soundPlayer.stop()
+def saveFile():
+    print("Here")
+    fname,_ = QFileDialog.getSaveFileName(None, 'Save file', '/home/daman1209arora/voice-recreator')
+    fl.writeInFile(fname,harmonicSliders)
+   
+def loadFile():
+    fname,_=QFileDialog.getOpenFileName(None,'Open file', '/home/daman1209arora/voice-recreator')
+    freq,hrmncs=fl.readFile(fname)
+    setSliderValues(freq,hrmncs)
 
-def play(q):
-    global isPlaying
-    if(isPlaying==False):
-        isPlaying=True
-        playSound()
-
-samplingRate=44100
-base_freq=100
-
+def setSliderValues(freq,values):
+    for i in range(0,len(values)):
+        harmonicSliders[i].setValue(values[i])
 
 def createSoundBuffer(base_freq):
     buffer=np.zeros(int(samplingRate/base_freq))
@@ -57,19 +101,6 @@ def createSoundBuffer(base_freq):
     buffer=buffer.astype(np.int16)
     return buffer
 
-def playSound():
-    global soundPlayer
-    buffer=createSoundBuffer(base_freq)
-    file = wave.open('temp.wav','w')
-    file.setnchannels(2)
-    file.setsampwidth(2)
-    file.setframerate(sampleRate)
-    for v in buffer:
-        file.writeframesraw(struct.pack('<h',v ))
-    file.close()
-    soundPlayer=pygame.mixer.Sound('temp.wav')
-    soundPlayer.play(-1)
-    
 
 def createHarmonics(freq,n): 
     harmonicWindow=QWidget(mainWindow)
@@ -84,15 +115,16 @@ def createHarmonics(freq,n):
         slider.setTickInterval(tick_interval) 
         slider.setMinimum(interval[0]) 
         slider.setMaximum(interval[1]) 
-        if(i==0):
-            slider.setValue(interval[1])
-        else:
-            slider.setValue(interval[0])
-        slider.valueChanged.connect(playSound)
+        slider.sliderReleased.connect(play)
         harmonicSliders.append(slider)
         layout.addWidget(slider)
         box.setLayout(layout)
         main_layout.addWidget(box)
+
+    values=np.full(n,interval[0])
+    values[0]=interval[1]
+
+    setSliderValues(base_freq,values)
 
     harmonicWindow.setLayout(main_layout)
     harmonics.setWidget(harmonicWindow)
@@ -113,13 +145,14 @@ def createFileSystem():
     saveButton,loadButton=QPushButton('Save'),QPushButton('Load')
     saveButton.setIcon(QIcon(QPixmap('savebutton.png')))
     loadButton.setIcon(QIcon(QPixmap('loadbutton.jpeg')))
+    saveButton.clicked.connect(saveFile)
+    loadButton.clicked.connect(loadFile)
     layout=QHBoxLayout()
     layout.addWidget(saveButton)
     layout.addWidget(loadButton)
     fileWindow.setLayout(layout)
 
 def createMainWindow():
-    global base_freq
     layout=QVBoxLayout()
     layout.addWidget(harmonics)
     layout.addWidget(fileWindow)
